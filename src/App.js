85c086bbcca8ef0a6479ddd7fd3dcf5c8d45e70b
LiamBotha <LiamB2010@gmail.com>
2021-08-23 14:19:05 +0200

    Finshed Adding Backend needs some polish

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useDebugValue, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import EndPopup from './components/EndPopup';
 import HitPoint from './components/HitPoint';
@@ -9,22 +9,28 @@ import jokerImg from './img/Joker.png';
 import promptoImg from './img/Prompto.png';
 import nierImg from './img/Nier.png';
 import StartPopup from './components/StartPopup';
+import firebase from './components/Firebase';
 
 const photoStyle = { width: '100vw', objectFit: 'cover' };
 
 function App() {
   let [targets, setTargets] = useState([
-    {name: 'Joker', posX: 55, posY: 53, threshold: 4, img: jokerImg},
-    {name: '2B', posX: 25, posY: 65, threshold: 4, img: nierImg},
-    {name: 'Prompto', posX: 22.7, posY: 43, threshold: 4, img: promptoImg},
+    {name: 'Joker', img: jokerImg},
+    {name: '2B', img: nierImg},
+    {name: 'Prompto', img: promptoImg},
   ]);
   let [foundTargets, setFoundTargets] = useState([]);
   let [curTarget, setCurTarget] = useState({id: 0 });
+
   let [bisStarted, setIsStarted] = useState(false);
   let [bIsCompleted, setIsCompleted] = useState(false);
+  console.log('refreshing completed', bIsCompleted);
+  
   let [time, setTime] = useState(0);
   let [timeInterval, setTimeInterval] = useState(null);
   let [completedTime, setCompletedTime] = useState(-1);
+
+  let [userId, setUserId] = useState('');
 
   const StartGame = () => {
     setIsStarted(true);
@@ -32,14 +38,22 @@ function App() {
     const startTime = Date.now();
     let interval = setInterval(() => setTime(Date.now() - startTime), 10);
     setTimeInterval(interval);
-  }
 
+    const id = firebase.firestore().collection('UsersA').doc().id
+    firebase.firestore().collection('UsersA').doc(id).set({ Name: 'Placeholder', TimeScore: 'N/A', startTime });
+    setUserId(id);
+  }
+  
   useEffect(() => {
     if(targets.length === 0)
     {
-      setIsCompleted(true);
-      setCompletedTime(time);
-      clearInterval(timeInterval);
+      const ref = firebase.firestore().collection('UsersA').doc(userId);
+      ref.get().then((item) => {
+        let val = Date.now() - item.data().startTime;
+          setIsCompleted(true);
+          setCompletedTime(val);
+          clearInterval(timeInterval);
+      })
     }
   }, [targets]);
 
@@ -48,18 +62,25 @@ function App() {
     let y = (e.pageY / e.target.height) * 100;
     console.log(`Mouse: ${x}, ${y}`);
     if(targets.length > 0)
-    {
+    { 
       let hitTarget = targets[curTarget.id];
-      let bWithinXRange = Math.abs(hitTarget.posX - x) < hitTarget.threshold;
-      let bWithinYRange = Math.abs(hitTarget.posY - y) < hitTarget.threshold;
-      if(bWithinXRange && bWithinYRange)
-      {
-        setFoundTargets(prevState => [...prevState, hitTarget]);
-        let newTargets = [...targets];
-        newTargets.splice(curTarget.id, 1);
-        setTargets(newTargets);
-        setCurTarget({id: 0});
-      }
+      const ref = firebase.firestore().collection('PhotoA').doc(hitTarget.name);
+      ref.onSnapshot((querySnapshot) => {
+        const posX = querySnapshot.data().PosX;
+        const posY = querySnapshot.data().PosY;
+        const threshold = querySnapshot.data().Threshold;
+
+        let bWithinXRange = Math.abs(posX - x) < threshold;
+        let bWithinYRange = Math.abs(posY - y) < threshold;
+        if(bWithinXRange && bWithinYRange)
+        {
+          setFoundTargets(prevState => [...prevState, {name: hitTarget.name, posX, posY, threshold}]);
+          let newTargets = [...targets];
+          newTargets.splice(curTarget.id, 1);
+          setTargets(newTargets);
+          setCurTarget({id: 0});
+        }
+      });
     }
   };
   return (
@@ -72,7 +93,7 @@ function App() {
       { 
         foundTargets.map((x) => <HitPoint point={x}/>)
       }
-      { bIsCompleted && <EndPopup time={completedTime}/> }
+      { bIsCompleted && <EndPopup time={completedTime} userId={userId}/> }
     </div>
   );
 }
--- src/components/EndPopup.js
+++ src/components/EndPopup.js
@@ -1,3 +1,6 @@
+import { Fragment, useEffect, useState } from 'react';
+import firebase from './Firebase';
+
 const popupStyle = {
     position: 'fixed',
     left:0,
@@ -16,22 +19,67 @@ const popupStyle = {
 }
 const scoreStyle = {
     listStyle: 'none',
-    padding: 0,
+    padding: '0 1rem 0.3rem 1rem',
+    width: '100%',
+    textAlign: 'center',
+}
+const getScores = (setScores) =>  {
+    const ref = firebase.firestore().collection('UsersA').orderBy('TimeScore').limit(10);
+    ref.onSnapshot((querySnapshot) => {
+        let items = [];
+        querySnapshot.forEach((doc) => {
+            if(doc.data().Name !== 'Placeholder' && doc.data().TimeScore !== 'N/A')
+            items.push(doc.data());
+        });
+        setScores(items);
+    });
 }
 
+const ScoreList = (props) => {
+    return <div>
+        <h2>Highscores: </h2>
+        <table id='score-list' style={scoreStyle}>
+        <th>Name</th>
+        <th>Time</th>
+        { 
+            props.scores.map((score) => <tr>
+                <td>{score.Name}</td>
+                <td>{score.TimeScore}</td>
+            </tr>)
+        }     
+        </table>
+    </div>
+};
+
 export default function EndPopup(props) {
+    let [scores, setScores] = useState([]);
+    let [name, setName] = useState('');
+    let [bNameEntered, setBNameEntered] = useState(false);
+
+    useEffect(() => {
+        getScores(setScores);
+    },[]);
+
+    const handleScoreSubmit = (userId, TimeScoreString) => {
+        setBNameEntered(true);
+        firebase.firestore().collection('UsersA').doc(userId).update({ Name: name, TimeScore: TimeScoreString});
+    };
+
     let timerTime = props.time;
     let seconds = ("0" + (Math.floor(timerTime / 1000) % 60)).slice(-2);
-    let minutes = (Math.floor(timerTime / 60000));
+    let minutes = ("0" + (Math.floor(timerTime / 60000) % 60)).slice(-2);
+    let hours = ("0" + Math.floor(timerTime / 3600000)).slice(-2);
+
     return <div style={popupStyle}>
         <h1>Game Completed!</h1>
-        <h2>You have found them all in {minutes < 10 ? '0' + minutes : minutes} : {seconds}</h2>
-        <h2>Highscores: </h2>
-        <ul id='score-list' style={scoreStyle}>
-            <li>1. Jon Ivy - 00:00:12</li>
-            <li>2. Jon Ivy - 00:00:12</li>
-            <li>3. Jon Ivy - 00:00:12</li>
-            <li>4. Jon Ivy - 00:00:12</li>
-        </ul>
+        <h2>You have found them all in {hours}:{minutes}:{seconds}</h2>
+        {
+            bNameEntered === false 
+                ? <div  style={{padding: '5rem 0 0 0'}}>
+                    <input value={name} placeholder='Your Name' onChange={(e) => setName(e.target.value)} type='text' />
+                    <button onClick={() => handleScoreSubmit(props.userId, `${hours}:${minutes}:${seconds}`)}>Submit</button> 
+                </div>
+                : <ScoreList scores={scores}/> 
+        }
     </div>
 }
